ad2191c6eb24ffb241d6cf6390d3c97895d434ed
nicolas-CaC <nicolas.machicado@bue.edu.ar>
2023-05-30 21:20:55 -0300

    Clase 1 - SQL

--- Clase 2 - a - Numericos.sql
+++ Clase 2 - a - Numericos.sql
@@ -1,1 +1,34 @@
 USE java23062;
+DESCRIBE numericos;
+
+DROP TABLE numericos;
+SELECT * FROM numericos;
+SHOW fields FROM numericos;
+
+
+CREATE TABLE numericos(tinysigned TINYINT);
+CREATE TABLE numericos(
+	tinyunsigned TINYINT UNSIGNED,
+	tinysigned TINYINT
+);
+INSERT INTO numericos VALUES (255, 127);
+
+
+
+
+CREATE TABLE numericos(
+	tipotiny TINYINT NOT NULL,
+	tiposmall SMALLINT,
+	tipomedium MEDIUMINT NOT NULL,
+	tipoint int,
+	tipobig bigint,
+	
+);
+
+INSERT INTO numericos VALUES (
+	127,
+	32767,
+	8388607,
+	2147483647,
+	9223372036854775807
+);
